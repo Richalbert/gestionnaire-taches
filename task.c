25aa5876cc4b0c6426f2794abdf40c862a76060a
Richalbert <richalbert@free.fr>
2025-01-25 18:22:13 +0100

    Ajout des fonctions de lecture et de sauvegarde dans un fichier

--- task.c
+++ task.c
@@ -7,7 +7,7 @@
  *
  * @author Richard
  * @date 2025-01-25
- * @version 0.1
+ * @version 0.3
  */
 
 #include <stdio.h>
@@ -94,5 +94,70 @@ void print_all_tasks(const TaskManager *manager) {
     for (int i = 0; i < manager->count; i++) {
         print_task(&manager->tasks[i]);
     }
+}
+
+/**
+ *  @brief Sauvegarde les taches dans un fichier
+ *
+ *  @param const TaskManager *manager : Pointeur sur le tableau des taches
+ *      const signifie au compilateur que les modifications sont interdites
+ *  @param const char *filename : nom du fichier
+ *
+ *  @return l'echec ou la reussite de la sauvegarde
+ */
+int save_tasks_to_file(const TaskManager *manager, const char *filename) {
+    FILE *file = fopen(filename, "w");
+    if (!file) {
+        perror("Erreur lors de l'ouverture du fichier");
+        return -1;
+    }
+
+    for (int i = 0; i < manager->count; i++) {
+
+        // const garanti aucune modification de la tache pointee par task
+        const Task *task = &manager->tasks[i];
+        fprintf(file, "%d;%s;%d\n", 
+            task->id, task->description, task->completed);
+    }
+
+    fclose(file);
+    return 0;
+}
+
+/**
+ *  @brief  Charge les taches depuis un fichier
+ *
+ *  @param  *manager : pointeur sur le tableau de tache
+ *  @param  *filename : nom du fichier
+ *
+ *  @return la reussite ou l'echec du chargement
+ */
+int load_tasks_from_file(TaskManager *manager, const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        printf("\nLe fichier n'existe pas\n");
+        return 0;
+    }
+
+    manager->count = 0;
+    while (!feof(file)) {
+        Task *task = &manager->tasks[manager->count];
+
+        if (strlen(task->description) >= 256) {
+            fprintf(stderr, "Erreur : description trop longue\n");
+            return -1;
+        }
+
+        if (fscanf(file, "%d;%255[^;];%d\n", 
+            &task->id,          //adresse ou stocker le 1er entier 
+            task->description,  //pointeur vers un tableau de caractere
+            &task->completed)   //adresse ou stocker le dernier entier
+            == 3) {     // fscanf rerourne le nb de champ lus
+            manager->count++;
+        }
+    }
+
+    fclose(file);
+    return 0;
 }
 
